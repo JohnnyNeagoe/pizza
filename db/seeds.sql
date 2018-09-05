aa3e299df42a2073851eb7d3a4315a53fdff999b
Johnny Neagoe <johnnyneagoe@gmail.com>
2018-09-04 23:28:25 -0400

    Structure/Functionality Base

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO pizzas (name) VALUES ('Large Peperonni');
+INSERT INTO pizzas (name) VALUES ('Cheesy');
+INSERT INTO pizzas (name, polish) VALUES ('Little Ceaser"s Stuffed Crust', true);
+INSERT INTO pizzas (name, polish) VALUES ('Canadian', true);
---
+++ schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS pizza_db;
+CREATE DATABASE pizza_db;
+
+
+USE pizza_db;
+
+CREATE TABLE `pizzas` (
+id int NOT NULL AUTO_INCREMENT,
+pizza_name varchar(255) NOT NULL,
+polished BOOLEAN DEFAULT FALSE,
+PRIMARY KEY (id)
+);
